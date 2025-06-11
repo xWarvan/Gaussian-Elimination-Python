c0e41981bbdefc180eb48eaad063a19f2b649f6b
warvan <122137078+xWarvan@users.noreply.github.com>
2025-06-11 20:06:10 +0700

    Update gaussian_elimination.py

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -2,18 +2,11 @@ import numpy as np
 import warnings
 
 def swapRows(A, i, j):
-    """
-    interchange two rows of A
-    operates on A in place
-    """
     tmp = A[i].copy()
     A[i] = A[j]
     A[j] = tmp
 
 def relError(a, b):
-    """
-    compute the relative error of a and b
-    """
     with warnings.catch_warnings():
         warnings.simplefilter("error")
         try:
@@ -22,10 +15,6 @@ def relError(a, b):
             return 0.0
 
 def rowReduce(A, i, j, pivot):
-    """
-    reduce row j using row i with pivot pivot, in matrix A
-    operates on A in place
-    """
     factor = A[j][pivot] / A[i][pivot]
     for k in range(len(A[j])):
         if np.isclose(A[j][k], factor * A[i][k]):
@@ -33,105 +22,68 @@ def rowReduce(A, i, j, pivot):
         else:
             A[j][k] = A[j][k] - factor * A[i][k]
 
-
-# stage 1 (forward elimination)
 def forwardElimination(B):
-    """
-    Return the row echelon form of B
-    """
     A = B.copy().astype(float)
     m, n = np.shape(A)
     for i in range(m-1):
-        # Let lefmostNonZeroCol be the position of the leftmost nonzero value 
-        # in row i or any row below it 
         leftmostNonZeroRow = m
         leftmostNonZeroCol = n
-        ## for each row below row i (including row i)
-        for h in range(i,m):
-            ## search, starting from the left, for the first nonzero
-            for k in range(i,n):
+        for h in range(i, m):
+            for k in range(i, n):
                 if (A[h][k] != 0.0) and (k < leftmostNonZeroCol):
                     leftmostNonZeroRow = h
                     leftmostNonZeroCol = k
                     break
-        # if there is no such position, stop
         if leftmostNonZeroRow == m:
             break
-        # If the leftmostNonZeroCol in row i is zero, swap this row 
-        # with a row below it
-        # to make that position nonzero. This creates a pivot in that position.
-        if (leftmostNonZeroRow > i):
+        if leftmostNonZeroRow > i:
             swapRows(A, leftmostNonZeroRow, i)
-        # Use row reduction operations to create zeros in all positions 
-        # below the pivot.
-        for h in range(i+1,m):
+        for h in range(i+1, m):
             rowReduce(A, i, h, leftmostNonZeroCol)
     return A
 
-#################### 
-
-# If any operation creates a row that is all zeros except the last element,
-# the system is inconsistent; stop.
 def inconsistentSystem(A):
-    """
-    B is assumed to be in echelon form; return True if it represents
-    an inconsistent system, and False otherwise
-    """
-    # remove the next line
-    #pass
     m, n = np.shape(A)
     for i in range(m):
-        nonZeros = np.nonzero(A[i])[0] #tracks indices of nonzero elements in array
+        nonZeros = np.nonzero(A[i])[0]
         if len(nonZeros) == 0:
-            continue  #returns row if it is all zeroes
-
-        if nonZeros[0]== n- 1 and len(nonZeros)==1:
-            return True #returns true if system is inconsistent
-    
-    return False #else returns false
-
-
-
+            continue
+        if nonZeros[0] == n - 1 and len(nonZeros) == 1:
+            return True
+    return False
 
 def backsubstitution(A):
-    """
-    return the reduced row echelon form matrix of B
-    """
-    # remove the next line
-    #pass
     m, n = np.shape(A)
-    A = A.copy().astype(float)  #copies orginal matrix float
-    
-    for i in range(m-1, -1, -1): #starts loop from bottom row
+    A = A.copy().astype(float)
+    for i in range(m-1, -1, -1):
         nonzeroindex = np.nonzero(A[i])[0]
         if len(nonzeroindex) == 0:
-            continue  #continues row if all elements are zeroes
+            continue
         pivot = nonzeroindex[0]
-                #pivot rows
         if pivot == n - 1:
-            continue #continues if pivot is in last col
-        A[i] = A[i] / A[i, pivot] 
-        # makes sure each element above pivot is zero
+            continue
+        A[i] = A[i] / A[i, pivot]
         for j in range(i):
             A[j] -= A[j, pivot] * A[i]
-    
     return A
 
-# A = np.array([[1,-3,2,-3],
-#               [5,4,6,41],
-#               [10,-9,8,-6]])
+# Matriks augmented untuk sistem:
+# 2x – 3y + 2z = 3
+# x  - y  -2z = -1
+# -x + 2y -3z = -4
 
-# A = np.loadtxt("h2m6.txt")
-              
-# A_1 = forwardElimination(A)
-# A_2 = backsubstitution(A_1)
+A = np.array([
+    [ 2, -3,  2,  3],
+    [ 1, -1, -2, -1],
+    [-1,  2, -3, -4]
+])
 
-# print(A_2)
-# print("inconsistent?:",inconsistentSystem(A_2))
+A_1 = forwardElimination(A)
+A_2 = backsubstitution(A_1)
 
+print("Row Echelon Form (A_1):\n", A_1)
+print("\nReduced Row Echelon Form (A_2):\n", A_2)
+print("\nInconsistent?:", inconsistentSystem(A_2))
 
-
-# print("Solution:", A_2[:, -1])
-
-#####################
-
+solution = A_2[:, -1]
+print("\nSolution (x, y, z):", solution)
